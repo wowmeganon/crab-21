5695f39b1d8b90ec88b61006fec26c0630e06906
svlieri <oriondo.abp@gmail.com>
2021-12-06 10:54:32 +0800

    Consume dependencies

---
+++ scaffold.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const app = express();
+const port = 8000;
+
+app.listen(port, () => console.log(`Listening on ${port}...`));
