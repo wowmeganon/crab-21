d8d858bc12280b62501e69c7c5ba67fcc38ea3e4
svlieri <oriondo.abp@gmail.com>
2021-12-06 11:02:51 +0800

    Log sample body

--- scaffold.js
+++ scaffold.js
@@ -6,5 +6,10 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const app = express();
 
+request('http://google.com', function(e, res, body) {
+  if(e) console.log('Error:', e);
+  console.log(body);
+})
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
