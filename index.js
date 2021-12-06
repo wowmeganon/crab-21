988fd1ef6b3c4440b5dc577b78bc7aa166a2016e
svlieri <oriondo.abp@gmail.com>
2021-12-06 13:56:20 +0800

    Rename to index

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const app = express();
+
+const url = process.env.URL || 'http://www.google.com';
+
+request(url, function(e, res, body) {
+  if(e) console.log('Error:', e);
+  console.log(body);
+});
+
+const dest = fs.createWriteStream('./Downloads/index.html');
+request(url).pipe(dest).on('finish', () => console.log('Done'));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
