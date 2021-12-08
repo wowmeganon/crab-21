734fbbf70f44e02fc427af008a576ece1d53bad2
svlieri <oriondo.abp@gmail.com>
2021-12-08 14:00:16 +0800

    Select array elements using cheerio

--- request.js
+++ request.js
@@ -5,9 +5,17 @@ const fs = require('fs');
 
 const url = process.env.NODE_URL;
 
-async function main (url) {
+async function main(url) {
     const html = await request.get(url);
     fs.writeFileSync('./request.html', html);
+
+    const $ = cheerio.load(html);
+    const innerText = $('h1').text();
+    console.log(innerText);
+
+    $('h2').each((index, element) => {
+        console.log(index, $(element).text());
+    });
 }
 
 main(url);
