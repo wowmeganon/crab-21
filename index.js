a52fd4e5d84c9527fa12dbefa16d1861dc784733
svlieri <oriondo.abp@gmail.com>
2021-12-09 13:03:24 +0800

    Add crawler schema

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
 const request = require('request-promise');
 const cheerio = require('cheerio');
+const fs = require('fs');
 const url = process.env.NODE_URL;
+
+const schema = {
+  position,
+  name,
+  location,
+  rate
+}
 
 async function main() {
   try {
     const html = await request.get(url);
     console.log(html)
+    fs.writeFileSync('./index.html', html);
   } catch(err) {
     console.error(err);
   }
