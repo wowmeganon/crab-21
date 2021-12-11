1dc10d833dad53f58c226d58ec0edf5ed0e9d4da
svlieri <oriondo.abp@gmail.com>
2021-12-11 11:03:23 +0800

    Push data to results

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const url = process.env.NODE_URL;
 
+const results = [];
+
 async function main() {
   try {
     const html = await request.get(url);
-    console.log(html)
-    fs.writeFileSync('./index.html', html);
-  } catch(err) {
-    console.error(err);
-  }
+    const $ = await cheerio.load(html);
+
+    $('[itemprop="itemListElement"]').each(function(index,element) {
+      const result = $(element).children('[itemprop="name"]');
+      const title = result.text();
+      const url = result.attr('href');
+
+      results.push({ title, url });
+    });
+
+  } catch(err) { console.error(`Error: ${err}`) }
 }
 main();
