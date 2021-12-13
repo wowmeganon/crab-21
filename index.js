3e3257cc0538210039b28ad872952730e0a81ad8
svlieri <oriondo.abp@gmail.com>
2021-12-13 18:02:18 +0800

    Fix tabs

--- index.js
+++ index.js
@@ -10,27 +10,26 @@ async function main() {
     const html = await request.get(url);
     const $ = await cheerio.load(html);
 
-  $('.thumbnail').each((index, element) => {
-    const caption = $(element).children('.caption');
-    const price = caption.children('.price').text();
-    const name = caption.find('.title').text();
-    const desc = caption.children('.description').text();
+    $('.thumbnail').each((index, element) => {
+        const caption = $(element).children('.caption');
+        const price = caption.children('.price').text();
+        const name = caption.find('.title').text();
+        const summary = caption.children('.description').text();
 
-    let link = caption.find('.title').attr('href');
-    const getDomain = (url) => {
-        const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-        return matches && matches[1];
-    };
-    link = getDomain(url).concat(link);
+        let link = caption.find('.title').attr('href');
+        const getDomain = (url) => {
+            const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+            return matches && matches[1];
+        };
+        link = getDomain(url).concat(link);
 
-    const rating = $(element).children('.ratings');
-    const review = rating.children('p.pull-right').text();
-    const star = rating.children('p').attr('data-rating');
+        const rating = $(element).children('.ratings');
+        const review = rating.children('p.pull-right').text();
+        const star = rating.children('p').attr('data-rating');
 
-    const result = { name, price, desc, review, star, link };
-    results.push(result);
-    console.log(result);
-  });
-
+        const result = { name, price, summary, review, star, link };
+        results.push(result);
+    });
+    return results;
 }
 main();
