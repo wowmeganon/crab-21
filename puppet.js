5cf52e90bbbcbd20018f5da8702529b13e2183b3
svlieri <oriondo.abp@gmail.com>
2021-12-14 18:58:03 +0800

    Test puppeteer

--- puppet.js
+++ puppet.js
@@ -1,7 +1,15 @@
 
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const url = process.env.NODE_URL;
 
 async function main() {
-    await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const content = await page.content();
+  const $ = await cheerio.load(content);
+  console.log($('h1').text());
 }
 main();
