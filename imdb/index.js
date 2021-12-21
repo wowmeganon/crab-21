8e626f79313a04eded6ebab06990615942e5b573
svlieri <oriondo.abp@gmail.com>
2021-12-21 13:27:52 +0800

    Bug getaddrinfo ENOTFOUND

--- imdb/index.js
+++ imdb/index.js
@@ -1,7 +1,7 @@
 
 const request = require('request-promise');
 const cheerio = require('cheerio');
-const url = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm";
+const url = "http://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm";
 
 async function getTopMovies() {
   const html = await request.get(url);
@@ -11,19 +11,27 @@ async function getTopMovies() {
     const title = $(movie).find('.titleColumn > a').text();
     const year = $(movie).find('.titleColumn > span').text();
     const rating = $(movie).find('.imdbRating > strong').text();
+    const link = 'http://www.imdb.com' + $(movie).find('.posterColumn > a').attr('href');
 
-    let link = $(movie).find('.posterColumn > a').attr('href');
-    link = 'http://www.imdb.com' +link;
-
-    return ({ title, year, rating, rank: i, link });
+    return (title ? { title, year, rating, rank: i, link } : null);
   }).get();
+
   return movies;
 }
 
-async function getMoviesPoster() {
-  await Promise.all(movies.map(async (movie) => {
-
-  }))
+async function getMoviesPoster(movies) {
+  const posters = await Promise.all(movies.map(async (movie) => {
+    try {
+      const html = await request.get(movie.link);
+      const $ = await cheerio.load(html);
+      movie.poster = $('div').find('.ipc-media__img > img').attr('src');
+      return movie;
+    }
+    catch(err) {
+      console.log('Error:', err.message);
+    }
+  }));
+  return posters;
 }
 
 async function main() {
