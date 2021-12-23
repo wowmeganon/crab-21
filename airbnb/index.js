df2b4e768d83288b320fc67dae232e7ee91d763b
svlieri <oriondo.abp@gmail.com>
2021-12-23 11:25:09 +0800

    Launch puppeteer

--- airbnb/index.js
+++ airbnb/index.js
@@ -3,4 +3,10 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const url = process.env.NODE_URL;
 
-console.log(url);
+async function getHomes() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+}
+
+getHomes();
