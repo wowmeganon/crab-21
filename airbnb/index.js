d7565561fb46207b66ac5687593578b39b051288
svlieri <oriondo.abp@gmail.com>
2021-12-24 14:40:59 +0800

    Add waitUntil puppeteer option

--- airbnb/index.js
+++ airbnb/index.js
@@ -4,16 +4,19 @@ const cheerio = require('cheerio');
 const url = process.env.NODE_URL;
 
 async function getHomes() {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2" });
 
-  const html = await page.evaluate(() => document.body.innerHTML);
-  const $ = await cheerio.load(html);
+    const html = await page.evaluate(() => document.body.innerHTML);
+    const $ = await cheerio.load(html);
 
-  const homes = $("[itemprop='url']").map((i, home) => $(home).attr('content')).get();
-  return homes;
-
+    const homes = $("[itemprop='url']").map((i, home) => $(home).attr('content')).get();
+    return homes;
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 async function main() {
