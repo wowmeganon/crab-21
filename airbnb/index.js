d24184217882aec7835aa8788539d9834834e7a5
svlieri <oriondo.abp@gmail.com>
2021-12-24 12:51:26 +0800

    Fetch URL from homes

--- airbnb/index.js
+++ airbnb/index.js
@@ -7,6 +7,18 @@ async function getHomes() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
+
+  const html = await page.evaluate(() => document.body.innerHTML);
+  const $ = await cheerio.load(html);
+
+  const homes = $("[itemprop='url']").map((i, home) => $(home).attr('content')).get();
+  return homes;
+
 }
 
-getHomes();
+async function main() {
+  const homes = await getHomes();
+  console.log(homes);
+}
+
+main();
