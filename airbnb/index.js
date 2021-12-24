a33fb57414e7daefdd5adeef4302d9f8a536d93b
svlieri <oriondo.abp@gmail.com>
2021-12-24 15:31:39 +0800

    Add getDescription function

--- airbnb/index.js
+++ airbnb/index.js
@@ -3,26 +3,40 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const url = process.env.NODE_URL;
 
+let browser, page;
+
 async function getHomes() {
   try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2" });
 
     const html = await page.evaluate(() => document.body.innerHTML);
     const $ = await cheerio.load(html);
 
-    const homes = $("[itemprop='url']").map((i, home) => $(home).attr('content')).get();
-    await browser.close();
+    const homes = $("[itemprop='url']")
+          .map((i, home) => 'https://' +$(home).attr('content'))
+          .get();
     return homes;
   } catch(err) {
     console.error(err);
   }
 }
 
+async function getDescription(url) {
+  try {
+    await page.goto(url);
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
+
 async function main() {
-  const homes = await getHomes();
-  console.log(homes);
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  const homes = await getHomes(page);
+  for(i=0; i <homes.length; i++) {
+    await getDescription(homes[i], page);
+  }
 }
 
 main();
